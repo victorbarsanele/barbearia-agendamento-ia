7f4e3823e77fd3272a1e9930b3b121dea1cb3f68
Victor <victor.barsanele@gmail.com>
2026-08-15 10:11:03 -0300

    fix(prisma): remove unused email field in schema

---
+++ prisma/migrations/20260815124700_remove_email_cliente/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `email` on the `clientes` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "clientes" DROP COLUMN "email";
